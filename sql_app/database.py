3477d28b66137ed6c9ab68a25c6d7a691ef9c873
ehapsamy0 <ehapsamy434@gmail.com>
2023-03-29 23:29:31 +0200

    feat(docker): make dockrization to sql_app project

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy  import create_engine
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:nono1102001@localhost/User_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456789++@localhost/User_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
