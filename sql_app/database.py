3e0d01bbb00617cf345b053bb9860bda982d4ffd
ehapsamy0 <ehapsamy434@gmail.com>
2023-04-11 04:35:06 +0200

    feat(docker): make main with docker

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:nono1102001@localhost/User_db"
-
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://postgres:nono1102001@db:5432/User_db')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
